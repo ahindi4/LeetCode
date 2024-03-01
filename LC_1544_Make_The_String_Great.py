2db27f551cc1e8032d2fb3b8e525fba78c04e430
atefTECH <ahindi3@gatech.edu>
2024-02-29 21:55:55 -0600

    Pushing LC1544

---
+++ LC_1544_Make_The_String_Great.py
@@ -0,0 +1,20 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        
+        stack = []
+    
+        for char in s:
+            if stack and abs(ord(char) - ord(stack[-1])) == 32: 
+                stack.pop()
+            else:
+                stack.append(char)
+        
+        return ''.join(stack)
+
+
+#Line 7 code explination
+#Checking the ASCII values between two letters
+#if they are the same letter but one is capitlized 
+#then we subtract their values and check if they are equal to 32 
+#which is the difference between same letters if one is captlized
+#if the if statment is met we pop at stack[-1]
